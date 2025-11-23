2cf233ee0bfc4923fae019b459535534d7b10ba0
siamjshoishob1925-cloud <siam.j.shoishob1925@gmail.com>
2025-11-23 11:20:50 +0600

    Add files via upload

---
+++ FactorionUtil.java
@@ -0,0 +1,24 @@
+package Factorian;
+
+public class FactorionUtil {
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+    public static boolean isFactorion(int number) {
+        int original = number;
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += factorial(digit);
+            number /= 10;
+        }
+
+        return sum == original;
+    }
+}
